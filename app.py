996e95a660fb4816c5851589dffe8aaf4e30248a
Bello Soliu <95251843+techflow21@users.noreply.github.com>
2022-11-21 20:17:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,10 @@ import recommendation
 app = Flask(__name__)
 CORS(app)
 
-
 @app.route('/book', methods=['GET'])
 def recommend_books():
     res = recommendation.results(request.args.get('title'))
     return jsonify(res)
-
 
 if __name__ == '__main__':
     app.run(port=5000, debug= False)
